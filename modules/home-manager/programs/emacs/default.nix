fe5183ad50869c6eba8ba9a394cfc20463f5187b
Aaron He <AaronHeDev@outlook.com>
2026-02-01 14:18:32 +1100

    .

--- modules/home-manager/programs/emacs/default.nix
+++ modules/home-manager/programs/emacs/default.nix
@@ -9,13 +9,10 @@
     REPO_URL="http://git.pwo101.top/Config/Emacs.git"
 
     if [ ! -d "$TARGET" ]; then
-      # 如果目录不存在，直接 clone
       ${pkgs.git}/bin/git clone "$REPO_URL" "$TARGET"
     else
-      # 如果已经存在，你可以选择自动 pull 最新的，或者干脆跳过
       echo "Emacs config already exists at $TARGET, skipping clone."
-      # 想自动更新可以取消下面这行的注释：
-      # cd "$TARGET" && ${pkgs.git}/bin/git pull
+      cd "$TARGET" && ${pkgs.git}/bin/git pull
     fi
   '';
 }
