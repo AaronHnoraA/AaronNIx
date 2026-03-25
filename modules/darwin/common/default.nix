3a40675f405833db64db0c866de5920fb37b821f
Aaron He <AaronHeDev@outlook.com>
2026-03-25 21:41:41 +1100

    .

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -190,7 +190,7 @@
         showhidden = true;               # 隐藏的应用图标半透明
         persistent-apps = [
         {app = "/Applications/Brave Browser.app";}
-        {app = "/Users/hc/Applications/Home Manager Apps/kitty.app";}
+        {app = "/Applications/kitty.app";}
         {app = "/Users/hc/Applications/Home Manager Apps/Emacs.app";}
         {app = "/Applications/TradingView.app";}
         {app = "/Applications/MarginNote 4.app";}
--- modules/home-manager/programs/kitty/default.nix
+++ modules/home-manager/programs/kitty/default.nix
@@ -1,15 +1,4 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
-  config = lib.mkIf pkgs.stdenv.isDarwin {
-    home.packages = [ 
-      pkgs.kitty 
-    ];
-
-    # 3. 配置 kitty
-    # 将当前目录下的 ./kitty 文件夹链接到 ~/.config/kitty
-    xdg.configFile.kitty = {
-      source = ./kitty;
-      recursive = true; # 递归链接内部文件，方便后续混合管理或避免目录被只读锁定
-    };
-  };
+  # kitty is managed outside Nix/Home Manager on macOS.
 }
