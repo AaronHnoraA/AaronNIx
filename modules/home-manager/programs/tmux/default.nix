7851e0b57e5cd4a89566ffc63008ca18caadc402
Aaron He <AaronHeDev@outlook.com>
2026-04-15 21:24:45 +1000

    .

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -1,14 +1,19 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
     home.packages = [ 
       pkgs.tmux
     ];
 
-    # 3. 配置 kitty
     xdg.configFile.tmux = {
       source = ./tmux;
-      recursive = true; # 递归链接内部文件，方便后续混合管理或避免目录被只读锁定
+      recursive = true;
     };
+
+    home.file.".tmux.conf".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/tmux/tmux.conf";
+
+    home.file.".tmux.conf.local".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/tmux/tmux.conf.local";
   };
 }
